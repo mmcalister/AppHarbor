f3c2859e6aa2ec00103c91d55f722119d33aaa09
Matthew McAlister <mmcali@gmail.com>
2012-10-30 15:14:40 -0500

    test

--- AppHarbor/Controllers/HomeController.cs
+++ AppHarbor/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace AppHarbor.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump start your ASP.NET MVC application.";
+            ViewBag.Message = "Home of the Mom Locator";
 
             return View();
         }
