29eb08bb33cfe63cd3ef7da25265982fc1d64d2f
Matthew McAlister <mmcali@gmail.com>
2012-11-16 21:31:52 -0600

    updated to use the app harbor database.  added ability to create a person record.

---
+++ AppHarbor/Controllers/PeopleController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppHarbor.Models;
+
+namespace AppHarbor.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private PersonDBContext db = new PersonDBContext();
+
+        //
+        // GET: /People/
+
+        public ActionResult Index()
+        {
+            return View(db.People.ToList());
+        }
+
+        //
+        // GET: /People/Details/5
+
+        public ActionResult Details(Guid? id = null)
+        {
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        //
+        // GET: /People/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /People/Create
+
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                person.Id = Guid.NewGuid();
+                db.People.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        //
+        // GET: /People/Edit/5
+
+        public ActionResult Edit(Guid? id = null)
+        {
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        //
+        // POST: /People/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
+        //
+        // GET: /People/Delete/5
+
+        public ActionResult Delete(Guid? id = null)
+        {
+            Person person = db.People.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        //
+        // POST: /People/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Person person = db.People.Find(id);
+            db.People.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AppHarbor/Models/PersonModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.ComponentModel;
+
+namespace AppHarbor.Models
+{
+    public class Person
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+        public Address HomeAddress { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email Address")]
+        public string EmailAddress { get; set; }
+    }
+
+    public class Address
+    {
+        [DisplayName("Street")]
+        public string Line1         { get; set; }
+        [DisplayName("Appartment Number")]
+        public string Line2         { get; set; }
+        public string City          { get; set; }
+        public string State         { get; set; }
+        [DisplayName("Postal Code")]
+        public string PostalCode    { get; set; }
+        public string Country       { get; set; }
+    }
+
+    public class PersonDBContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+    }
+}
